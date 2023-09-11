c272da81070d8620becc4f1e8c41a2f6ee12b5ab
Manoj <manoj09633@gmail.com>
2023-09-11 16:02:48 +0530

    react form state registered for fields

--- src/components/Forms/form hook/LoginForm.jsx
+++ src/components/Forms/form hook/LoginForm.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export const LoginForm = () => {
+  const form = useForm();
+  const { register } = form;
   return (
     <div>
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" />
+        <input type="text" id="username" {...register("username")} />
 
         <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" />
+        <input type="text" {...register("email")} id="email" />
 
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input type="password" {...register("password")} id="password" />
 
         <button type="submit">Submit</button>
       </form>
