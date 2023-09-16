ee14bbbcbf81aca0d7802669a52328ee596f4fa0
Manoj <manoj09633@gmail.com>
2023-09-16 23:21:44 +0530

    Extracting URL params

--- src/components/Router/UserDetails.jsx
+++ src/components/Router/UserDetails.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export const UserDetails = () => {
-  return <div>UserDetails</div>;
+  const params = useParams();
+  const userId = params.userId;
+  return (
+    <>
+      <div>User Details of User {userId}</div>
+    </>
+  );
 };
