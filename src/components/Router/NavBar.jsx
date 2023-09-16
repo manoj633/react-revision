b5e66bcf27edda0fc98d0f655bad38329350fe56
Manoj <manoj09633@gmail.com>
2023-09-16 22:21:10 +0530

    Styling active link through CSS in JS

--- src/components/Router/NavBar.jsx
+++ src/components/Router/NavBar.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
 export const NavBar = () => {
+  const enableActiveLink = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? "bold" : "",
+      textDecoration: isActive ? "none" : "underline",
+    };
+  };
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
+      <NavLink style={enableActiveLink} to="/">
+        Home
+      </NavLink>
+      <NavLink style={enableActiveLink} to="/about">
+        About
+      </NavLink>
     </nav>
   );
 };
